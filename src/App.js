fa351c87498a01138ef3ec98191c5c8d7681725e
moko58 <r_chakra@yahoo.com>
2020-06-07 20:54:55 +0530

    List

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import LoadingSpinner from './LoadingSpinner';
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
+  const [images, setImages] = useState([]);
+
   console.log('value of isLoading', isLoading);
   if (!isLoading) {
     var requestOptions = {
@@ -17,17 +19,18 @@ function App() {
     setIsLoading(true);
     console.log('Before fetch');
     fetch('http://127.0.0.1:5000/list_of_images', requestOptions)
-      .then((response) => response.text())
+      .then((response) => response.json())
 
-      .then((result) => console.log(result))
-
-      .catch((error) => console.log('error', error))
-
-      
-      
+      .then((result) => {
+        console.log('results', result.data);
+        setImages([...images, ...result.data]);
+        console.log('images', images);
+      })
+      .catch((err) => {
+        console.log(err);
+        return null;
+      });
   }
-
- 
 
   return (
     <div>
@@ -38,7 +41,9 @@ function App() {
       <h1>images</h1>
 
       <div>{!isLoading ? <LoadingSpinner /> : null}</div>
-      <div className="box"></div>
+      <div className="box">
+        <DisplayImages images={images} />
+      </div>
     </div>
   );
 }
